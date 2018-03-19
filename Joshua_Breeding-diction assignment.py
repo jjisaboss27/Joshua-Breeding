6812de14cbe4764f74a468fef3b56d515e31dcb6
Joshua James Breeding <joshua.breeding2@stu.jefferson.kyschools.us>
2018-03-19 08:19:35 -0400

    second assignment

---
+++ Joshua_Breeding-diction assignment.py
@@ -0,0 +1,8 @@
+anime = {
+    'naruto': " written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, an adolescent ninja.",
+    'deathnote': "written by Tsugumi Ohba and illustrated by Takeshi Obata. The story follows Light Yagami, a high school student who discovers a supernatural notebook.",
+    'seven deadly sins': "written and illustrated by Nakaba Suzuki. It has been serialized in Kodansha's Weekly Shōnen Magazine since October 2012.",
+    'hellsing ultimate': "written and illustrated by Kouta Hirano. It chronicles the efforts of the mysterious and secret Hellsing Organization.",
+    'jojos bizarre adventure': "written and illustrated by Hirohiko Araki. It was originally serialized in Weekly Shōnen Jump from 1987 to 2004",
+    'the last air bender': "written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's Weekly Shōnen Jump magazine since July 22, 1997."
+}
